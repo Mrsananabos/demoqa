4b7ce898e693cb3848dd66a6a9aff14d5bc0b0c0
Shveytser-A <Shveytser-A@gaz-is.ru>
2022-09-27 21:31:48 +0300

    add allure attachments

--- src/test/java/com/demoqa/base/TestBase.java
+++ src/test/java/com/demoqa/base/TestBase.java
@@ -2,13 +2,14 @@ package com.demoqa.base;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import com.demoqa.helpers.AllureAttach;
+import com.demoqa.allure.AllureAttach;
 import com.demoqa.testData.TestData;
 import com.google.common.collect.ImmutableMap;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
@@ -34,6 +35,20 @@ public class TestBase {
 
     @BeforeAll
     static void configure() {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        Configuration.browserCapabilities = capabilities;
+        if (System.getProperty("remote") != null) {
+            Configuration.remote = System.getProperty("remote");
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+        }
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser_name", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1180");
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.baseUrl = "https://demoqa.com";
